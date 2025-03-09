292f16763779d59f845514bbd721f237ebc01092
Aaron <aaronjtrent@gmail.com>
2025-03-09 18:12:18 -0400

    can I commit? b

--- linear_algbra_setup.py
+++ linear_algbra_setup.py
@@ -25,3 +25,28 @@ print(xty)
 
 beta1, beta2 = np.matmul(inv_xtx, xty)
 print(beta1, beta2)
+
+#mlr example
+
+x = np.matrix([[1,1,12],
+               [1,2,15],
+               [1,3,20],
+               [1,4,22]])
+
+print(x)
+
+y = np.matrix([[1],
+               [3],
+               [3],
+               [5]])
+
+print(y)
+
+print(np.linalg.det(np.matmul(np.transpose(x), x)))
+
+inv_xtx = np.linalg.inv(np.matmul(np.transpose(x), x))
+xty = np.matmul(np.transpose(x), y)
+print(xty)
+
+beta1, beta2, beta3 = np.matmul(inv_xtx, xty)
+print(beta1, beta2, beta3)
